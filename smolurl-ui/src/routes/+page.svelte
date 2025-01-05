33e39028323c8cb46db65de260760a8c72c252b7
sxeix <sxeixarts@gmail.com>
2025-01-05 01:29:57 +0000

    Introduce basic svelte UI - need to adapt both services for production

---
+++ smolurl-ui/src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+    let userUrl = "";
+
+    let shortenUrl = "";
+
+    async function generateUrl() {
+        const payload = {
+            url: userUrl
+        };
+        const response = await fetch('http://localhost:8080/create', {
+            method: 'POST',
+            body: JSON.stringify(payload),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    
+        const { id } = await response.json();
+    
+        shortenUrl = window.location.href + `${id}`;
+        shortenUrl = `http://localhost:8080/${id}`;
+    }
+
+</script>
+
+<h1>Welcome to smolurl!</h1>
+<input bind:value={userUrl} placeholder="Please enter your url"/>
+<button on:click={generateUrl}>Generate</button>
+<h2>Your url is:</h2>
+<div>
+    <a href={shortenUrl}>{shortenUrl}</a>
+</div>
