aa3d9a90af41cab8dc594ccb353e5917e53d930f
sxeix <sxeixarts@gmail.com>
2025-04-13 17:47:16 +0100

    add basic docker orchestration

---
+++ build.sh
@@ -0,0 +1,7 @@
+mvn clean install -f smolurl
+
+docker build -t smolurl-backend smolurl
+
+docker build -t smolurl-frontend smolurl-ui
+
+docker-compose --env-file .env up --build
--- smolurl/src/main/java/org/sxeix/smolurl/config/CorsConfig.java
+++ smolurl/src/main/java/org/sxeix/smolurl/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // Replace with property
+                .allowedOrigins("http://localhost:4173")
                 .allowedMethods("POST")
                 .allowedHeaders("*");
     }
